451d9a9511865025a6c9255c49f8119f666fe624
Taren <Taren@DESKTOP-9FUS5IC>
2015-11-14 10:09:03 -0500

    ddd

--- hola/src/hola/Hello.java
+++ hola/src/hola/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo casa");
-		System.out.println("chao mundo  jajaja");
+		System.out.println("chao mundo  jajajaja");
 
 	}
 
